eda65775fa33816b11d31ae5f6e03636152d22f5
Tony <cjcontact86@gmail.com>
2020-04-18 21:08:19 +0530

    Adding shell script to automate git commit

---
+++ commit_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Author : Chethan C
+# Script to commit & push data to GIT repository
+
+#echo $1
+
+# To add any missing/updated file
+git add .
+
+# To commit data locally along with custom message as argument
+git commit -m "$1"
+
+# To push committed data to GIT master repository
+git push
